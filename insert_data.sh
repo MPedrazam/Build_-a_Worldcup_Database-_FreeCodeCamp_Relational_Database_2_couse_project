8882048b714d8096cccad73d531d7bd4138f12b1
Manuel Pedraza <111818883+MPedrazam@users.noreply.github.com>
2022-11-14 09:29:02 -0500

    Update insert_data.sh

--- insert_data.sh
+++ insert_data.sh
@@ -30,23 +30,15 @@ do
       
     elif [[ -z $TEAM_ID_2 ]]
     then
-      INSERT_WIN=$($PSQL "INSERT INTO teams(name) VALUES ('$OPPONENT')")
-        
+      INSERT_WIN=$($PSQL "INSERT INTO teams(name) VALUES ('$OPPONENT')")  
     fi
-    # Get new team_id
-    TEAM_ID_1=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
-    TEAM_ID_2=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
-    echo $TEAM_ID_2
     
     # Inseting the data of the games table
     if [[ $YEAR != "year" ]]
     then
-      INSERT_YEAR=$($PSQL "INSERT INTO games(year) VALUES ($YEAR)")
-      INSERT_ROUND=$($PSQL "INSERT INTO games(round) VALUES ('$ROUND')")
-      INSERT_OPP_G=$($PSQL "INSERT INTO games(opponent_goals) VALUES ($OPPONENT_GOALS)")
-      INSERT_WIN_G=$($PSQL "INSERT INTO games(winner_goals) VALUES ($WINNER_GOALS)")
-      INSERT_TEAM_WIN=$($PSQL "INSERT INTO games(winner_id) VALUES ($TEAM_ID_1)")
-      INSERT_TEAM_OPP=$($PSQL "INSERT INTO games(opponent_id) VALUES ($TEAM_ID_2)")
+      TEAM_ID_1=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+      TEAM_ID_2=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+      INSERT_GAMES_DATA=$($PSQL "INSERT INTO games(year, round, opponent_goals, winner_goals, winner_id, opponent_id) VALUES ($YEAR, '$ROUND', $OPPONENT_GOALS, $WINNER_GOALS, $TEAM_ID_1, $TEAM_ID_2)")
     fi
   fi 
 
