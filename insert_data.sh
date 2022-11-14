f5328875840d988229866baa0cf1fdf36ff83974
Manuel Pedraza <111818883+MPedrazam@users.noreply.github.com>
2022-11-14 11:40:55 -0500

    Update insert_data.sh

--- insert_data.sh
+++ insert_data.sh
@@ -12,7 +12,6 @@ fi
 echo $($PSQL "TRUNCATE teams, games")
 
 cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
-
 do
 
 # Inserting the teams (winners and opponents) info into table teams.
@@ -21,25 +20,26 @@ do
    # get team_id
     TEAM_ID_1=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
     TEAM_ID_2=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
-
+  
     # if no found
     if [[ -z $TEAM_ID_1 ]]
     # insert team
     then
-        INSERT_WIN=$($PSQL "INSERT INTO teams(name) VALUES ('$WINNER')")
-      
-    elif [[ -z $TEAM_ID_2 ]]
-    then
-      INSERT_WIN=$($PSQL "INSERT INTO teams(name) VALUES ('$OPPONENT')")  
+        INSERT_WIN=$($PSQL "INSERT INTO teams(name) VALUES ('$WINNER')")   
     fi
-    
-    # Inseting the data of the games table
-    if [[ $YEAR != "year" ]]
-    then
-      TEAM_ID_1=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
-      TEAM_ID_2=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
-      INSERT_GAMES_DATA=$($PSQL "INSERT INTO games(year, round, opponent_goals, winner_goals, winner_id, opponent_id) VALUES ($YEAR, '$ROUND', $OPPONENT_GOALS, $WINNER_GOALS, $TEAM_ID_1, $TEAM_ID_2)")
-    fi
-  fi 
 
- done
+    if [[ -z $TEAM_ID_2 ]]
+    then
+      INSERT_OPP=$($PSQL "INSERT INTO teams(name) VALUES ('$OPPONENT')")
+    fi
+  fi
+
+  # Inseting the data of the games table
+  if [[ $YEAR != "year" ]]
+
+  then
+    TEAM_ID_1=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+    TEAM_ID_2=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+    INSERT_GAMES_DATA=$($PSQL "INSERT INTO games(year, round, opponent_goals, winner_goals, winner_id, opponent_id) VALUES ($YEAR, '$ROUND', $OPPONENT_GOALS, $WINNER_GOALS, $TEAM_ID_1, $TEAM_ID_2)")
+  fi
+done
