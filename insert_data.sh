0f264fbfd0f93454c9b43a880ea6bd1861f1a2bf
Manuel Pedraza <111818883+MPedrazam@users.noreply.github.com>
2022-11-13 23:45:16 -0500

    Update insert_data.sh

--- insert_data.sh
+++ insert_data.sh
@@ -1,3 +1,5 @@
+#! /bin/bash
+
 if [[ $1 == "test" ]]
 then
   PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
@@ -10,3 +12,30 @@ fi
 echo $($PSQL "TRUNCATE teams, games")
 
 cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+
+do
+
+# Inserting the teams (winners and opponents) info into table teams.
+  if [[ $WINNER != "winner" ]]
+  then
+   # get team_id
+    TEAM_ID_1=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+    TEAM_ID_2=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+
+    # if no found
+    if [[ -z $TEAM_ID_1 ]]
+    # insert team
+    then
+        INSERT_WIN=$($PSQL "INSERT INTO teams(name) VALUES ('$WINNER')")
+      
+    elif [[ -z $TEAM_ID_2 ]]
+    then
+      INSERT_WIN=$($PSQL "INSERT INTO teams(name) VALUES ('$OPPONENT')")
+        
+    fi
+    # Get new team_id
+    TEAM_ID_1=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+    TEAM_ID_2=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+  fi 
+
+ done
